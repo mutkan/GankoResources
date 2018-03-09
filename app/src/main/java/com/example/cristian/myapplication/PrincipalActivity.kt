ccb5d08bbe281159cc070ba2899d25deb7632e37
Cristian Muñoz <cristian9016@gmail.com>
2018-03-09 10:05:10 -0500

    layouts designed

--- app/src/main/java/com/example/cristian/myapplication/PrincipalActivity.kt
+++ app/src/main/java/com/example/cristian/myapplication/PrincipalActivity.kt
@@ -2,11 +2,15 @@ package com.example.cristian.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class PrincipalActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
+
 }
