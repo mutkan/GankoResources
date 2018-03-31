cb251ef17d9f6704b783f6f20b9d0b2a0b7c652f
Ana Marin <anamarin@unicauca.edu.co>
2018-03-31 12:40:35 -0500

    Pull to master

--- app/src/main/java/com/example/cristian/myapplication/FilterFragment.kt
+++ app/src/main/java/com/example/cristian/myapplication/FilterFragment.kt
@@ -1,63 +1,6 @@
 package com.example.cristian.myapplication
 
-import android.content.Context
-import android.net.Uri
-import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [FilterFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- */
-class FilterFragment : Fragment() {
-
-    private var mListener: OnFragmentInteractionListener? = null
-
-
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_filter, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
-    }
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            mListener = context
-        } else {
-            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-}// Required empty public constructor
+class FilterFragment : Fragment() {}
