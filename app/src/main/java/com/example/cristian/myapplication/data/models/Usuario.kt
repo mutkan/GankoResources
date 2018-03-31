9ade0782fda5bee8ca43f4b3d95bf20e7cb3d1b2
Ana Marin <anamarin@unicauca.edu.co>
2018-03-31 15:08:52 -0500

    Login view model and client implemented

--- app/src/main/java/com/example/cristian/myapplication/data/models/Usuario.kt
+++ app/src/main/java/com/example/cristian/myapplication/data/models/Usuario.kt
@@ -10,4 +10,9 @@ class Usuario(
         var estado: String,
         var rol: String,
         var plan: String
-){}
+)
+
+class UserResponse(var id: Int,var username: String,var estado: String)
+class LoginResponse(var user:UserResponse,var token:String)
+class ResetPasswordResponse(var email:String,var estado:String)
+class ResetUserEmail(var email:String)
