c3b345005898c8a899f84f5b1cb73557c57c25a1
jlbeltran94 <jlbeltran94@gmail.com>
2018-05-24 19:11:02 -0500

    pre pull

---
+++ app/src/main/java/com/example/cristian/myapplication/ui/bovine/reproductive/add/AddDiagnosisActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cristian.myapplication.ui.bovine.reproductive.add
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddDiagnosisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_diagnosis)
+    }
+}
