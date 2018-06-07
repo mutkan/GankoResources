337a4442f2596eb023fb4674c642c65f80023810
Cristian Muñoz <cristian9016@gmail.com>
2018-06-06 19:06:43 -0500

    meadows in progress, not working yet

--- app/src/main/java/com/example/cristian/myapplication/ui/menu/MenuNavigation.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/menu/MenuNavigation.kt
@@ -8,8 +8,8 @@ import com.example.cristian.myapplication.ui.farms.FarmActivity
 import com.example.cristian.myapplication.ui.menu.Straw.StrawFragment
 import com.example.cristian.myapplication.ui.menu.bovine.ListBovineFragment
 import com.example.cristian.myapplication.ui.menu.management.ManageFragment
+import com.example.cristian.myapplication.ui.menu.meadow.MeadowFragment
 import com.example.cristian.myapplication.util.putFragment
-import kotlinx.android.synthetic.main.activity_menu.view.*
 import org.jetbrains.anko.startActivity
 import javax.inject.Inject
 
@@ -52,8 +52,8 @@ class MenuNavigation @Inject constructor(val activity: MenuActivity, val sesion:
 
     }
 
-    fun navigateToPrairies() {
-
+    fun navigateToMeadow() {
+        activity.putFragment(R.id.content_frame, MeadowFragment.instance())
     }
 
     fun navigateToVaccination() {
---
+++ app/src/main/java/com/example/cristian/myapplication/ui/menu/Movement/MovementActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cristian.myapplication.ui.menu.Movement
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cristian.myapplication.R
+
+class MovementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movement)
+        title = "Movimiento"
+
+    }
+}
