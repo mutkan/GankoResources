d0eea09fdf9db1a06f242b14cd6e088b4fed4309
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-06-14 20:10:32 -0500

    Groups module implemented

--- app/src/main/java/com/example/cristian/myapplication/ui/groups/GroupViewModel.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/groups/GroupViewModel.kt
@@ -8,6 +8,7 @@ import com.example.cristian.myapplication.data.preferences.UserSession
 import com.example.cristian.myapplication.util.andEx
 import com.example.cristian.myapplication.util.applySchedulers
 import com.example.cristian.myapplication.util.equalEx
+import com.example.cristian.myapplication.util.inEx
 import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
@@ -42,6 +43,9 @@ class GroupViewModel @Inject constructor(private val db: CouchRx,
         return db.listByExp("finca" equalEx  session.farmID andEx ("retirado" equalEx false), Bovino::class, pageSize, skip)
                 .applySchedulers()
     }
+
+    fun listSelected(ids:List<String>):Single<List<Bovino>> =
+            db.listByExp("finca" equalEx session.farmID andEx ("retirado" equalEx false) andEx ("_id" inEx  ids), Bovino::class)
 
 }
 
