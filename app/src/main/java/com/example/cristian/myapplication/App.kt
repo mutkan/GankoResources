584604bf6ebe8d0464923e8f125763c8c4e4a06b
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-07-04 10:23:07 -0500

    Notification Work implemented

--- app/src/main/java/com/example/cristian/myapplication/App.kt
+++ app/src/main/java/com/example/cristian/myapplication/App.kt
@@ -2,6 +2,9 @@ package com.example.cristian.myapplication
 
 import android.app.Activity
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.support.multidex.MultiDexApplication
 import com.example.cristian.myapplication.di.AppInjector
 import dagger.android.AndroidInjector
@@ -20,6 +23,22 @@ class App: MultiDexApplication(),HasActivityInjector{
         super.onCreate()
         AppInjector.init(this)
 
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "ganko.channel.notification"
     }
 
 }
---
+++ app/src/main/java/com/example/cristian/myapplication/work/NotificationWork.kt
@@ -0,0 +1,68 @@
+package com.example.cristian.myapplication.work
+
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import androidx.work.*
+import com.example.cristian.myapplication.App
+import com.example.cristian.myapplication.R
+
+import java.util.concurrent.TimeUnit
+
+
+class NotificationWork:Worker(){
+
+    override fun doWork(): Result {
+
+        val title = inputData.getString(ARG_TITLE, "Ganko")
+        val msg = inputData.getString(ARG_DESCRIPTION, null)
+        val id = inputData.getString(ARG_ID, null)
+        val type = inputData.getInt(ARG_TYPE, 0)
+
+        val icon = when(type){
+            TYPE_HEALTH -> R.drawable.ic_notify_hea
+            TYPE_MANAGEMENT -> R.drawable.ic_notify_man
+            else -> R.drawable.ic_notify_vac
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, App.CHANNEL_ID)
+                .setSmallIcon(icon)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .build()
+        NotificationManagerCompat.from(applicationContext)
+                .notify(102, notification)
+
+        return Result.SUCCESS
+    }
+
+    companion object {
+        private const val ARG_TITLE = ""
+        private const val ARG_ID = ""
+        private const val ARG_DESCRIPTION = ""
+        private const val ARG_TYPE = ""
+
+        const val TYPE_HEALTH = 0
+        const val TYPE_MANAGEMENT = 1
+        const val TYPE_VACCINES = 2
+
+        fun notify(type:Int, title:String, msg:String, docId:String, time:Long, timeUnit: TimeUnit){
+
+            val data: Data = mapOf(ARG_ID to docId,
+                    ARG_TITLE to title,
+                    ARG_DESCRIPTION to msg,
+                    ARG_TYPE to type)
+                    .toWorkData()
+
+            val notificationWork = OneTimeWorkRequestBuilder<NotificationWork>()
+                    .setInitialDelay(time, timeUnit)
+                    .setInputData(data)
+                    .build()
+
+            WorkManager.getInstance().enqueue(notificationWork)
+        }
+
+
+    }
+
+}
