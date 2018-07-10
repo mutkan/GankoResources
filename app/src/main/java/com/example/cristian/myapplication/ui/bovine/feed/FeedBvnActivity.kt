830d49c781a1142a68e767babf08e53349346a9d
Ana Marin <anamarin@unicauca.edu.co>
2018-07-09 19:35:52 -0500

    Health bovine complete

--- app/src/main/java/com/example/cristian/myapplication/ui/bovine/feed/FeedBvnActivity.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/bovine/feed/FeedBvnActivity.kt
@@ -3,6 +3,8 @@ package com.example.cristian.myapplication.ui.bovine.feed
 import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.example.cristian.myapplication.R
 import com.example.cristian.myapplication.di.Injectable
 import com.example.cristian.myapplication.ui.adapters.FeedBovineAdapter
@@ -10,6 +12,7 @@ import com.example.cristian.myapplication.util.LifeDisposable
 import com.example.cristian.myapplication.util.buildViewModel
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_list_feed_bovine.*
+import kotlinx.android.synthetic.main.activity_list_health_bovine.*
 import org.jetbrains.anko.toast
 import javax.inject.Inject
 
@@ -30,15 +33,17 @@ class FeedBvnActivity : AppCompatActivity(), Injectable {
         setContentView(R.layout.activity_list_feed_bovine)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle("Alimentacion")
-        recycler.adapter = adapter
+        recyclerFeedBovine.adapter = adapter
     }
 
     override fun onResume() {
         super.onResume()
-
+        recyclerFeedBovine.adapter= adapter
+        recyclerFeedBovine.layoutManager = LinearLayoutManager(this)
         dis add viewModel.getFeedBovine(idBovino)
                 .subscribeBy(
                         onSuccess = {
+                            if(it.isEmpty()) emptyFeedText.visibility = View.VISIBLE else emptyFeedText.visibility = View.GONE
                             adapter.feed = it
                         },
                         onError = {
