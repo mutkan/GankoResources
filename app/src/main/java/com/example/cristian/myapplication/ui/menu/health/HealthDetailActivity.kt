d4b81df2ee20305cf7d8ef5c96748171cf8e73dc
Ana Marin <anamarin@unicauca.edu.co>
2018-07-10 22:20:04 -0500

    Health detail activity created

---
+++ app/src/main/java/com/example/cristian/myapplication/ui/menu/health/HealthDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cristian.myapplication.ui.menu.health
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HealthDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health_detail)
+    }
+}
