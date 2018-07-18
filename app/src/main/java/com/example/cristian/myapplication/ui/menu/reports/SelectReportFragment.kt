2cfce05e99b8acc197d4b2e1efa719cd0ba0d609
Felipe Arcos <pipearcos221@gmail.com>
2018-07-17 22:15:21 -0500

    DataBinding in SelectReportFragment

--- app/src/main/java/com/example/cristian/myapplication/ui/menu/reports/SelectReportFragment.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/menu/reports/SelectReportFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cristian.myapplication.ui.menu.reports
 
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,14 +9,18 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.cristian.myapplication.R
+import com.example.cristian.myapplication.databinding.FragmentSelectReportBinding
 
 
 class SelectReportFragment : Fragment() {
 
+    lateinit var binding: FragmentSelectReportBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_report, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_select_report, container, false)
+        return binding.root
     }
 
     companion object {
