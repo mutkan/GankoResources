6f5c9692c8a32367ea64ef117ddb6897586a22a8
Felipe Arcos <pipearcos221@gmail.com>
2018-07-17 21:48:36 -0500

    viewPager, tabs and databinding in layout_reports_fragment

--- app/src/main/java/com/example/cristian/myapplication/ui/menu/reports/ReportsFragment.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/menu/reports/ReportsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cristian.myapplication.ui.menu.reports
 
 
+import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +9,18 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.cristian.myapplication.R
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
-class ReportsFragment : Fragment() {
+class ReportsFragment : Fragment(), HasSupportFragmentInjector {
+
+    @Inject
+    lateinit var injector: DispatchingAndroidInjector<Fragment>
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -17,9 +28,12 @@ class ReportsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_reports, container, false)
     }
 
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> = injector
+
     companion object {
         fun instance(): ReportsFragment = ReportsFragment()
     }
+
 
 
 }
