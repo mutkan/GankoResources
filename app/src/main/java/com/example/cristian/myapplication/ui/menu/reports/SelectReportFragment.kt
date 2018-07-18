8e79c41d6596dcc9d0536edc480ed0b20ea3b13b
Felipe Arcos <pipearcos221@gmail.com>
2018-07-17 21:13:34 -0500

    ReportsPagerAdapter implemented

--- app/src/main/java/com/example/cristian/myapplication/ui/menu/reports/SelectReportFragment.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/menu/reports/SelectReportFragment.kt
@@ -9,21 +9,17 @@ import android.view.ViewGroup
 
 import com.example.cristian.myapplication.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class SelectReportFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_select_report, container, false)
+    }
+
+    companion object {
+        fun instance(): SelectReportFragment = SelectReportFragment()
     }
 
 
