1b8e40258ecf9a33c2c85b71778e0057de3c5d87
Felipe Arcos <pipearcos221@gmail.com>
2018-10-23 21:38:48 -0500

    issues solved

--- app/src/main/java/com/example/cristian/myapplication/ui/bovine/BovineViewModel.kt
+++ app/src/main/java/com/example/cristian/myapplication/ui/bovine/BovineViewModel.kt
@@ -5,6 +5,7 @@ import com.example.cristian.myapplication.data.db.CouchRx
 import com.example.cristian.myapplication.data.models.Bovino
 import com.example.cristian.myapplication.data.preferences.UserSession
 import com.example.cristian.myapplication.util.applySchedulers
+import com.example.cristian.myapplication.util.equalEx
 import io.reactivex.Single
 import javax.inject.Inject
 
@@ -12,10 +13,17 @@ class BovineViewModel @Inject constructor(private val db: CouchRx, private val u
 
     fun getFarmId() = userSession.farmID
 
-    fun addBovine(bovino: Bovino): Single<String> =
-            db.insert(bovino)
+    fun addBovine(bovino: Bovino): Single<String> = checkId(bovino.codigo!!)
+            .flatMap { if(it) db.insert(bovino) else throw Throwable() }
                     .applySchedulers()
 
     fun updateBovine(idBovine: String, bovine:Bovino ) = db.update(idBovine, bovine).applySchedulers()
+
+    private fun checkId(id: String): Single<Boolean> =
+            db.oneByExp("codigo" equalEx id, Bovino::class)
+                    .map { false }
+                    .defaultIfEmpty(true)
+                    .toSingle()
+                    .applySchedulers()
 
 }
